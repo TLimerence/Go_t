b621a6c3fcbdffdb5820dde765096a31d848dcf2
root <1002853501@qq.com>
2024-12-17 14:57:13 +0800

    交一下试试

---
+++ test.go
@@ -0,0 +1,20 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+// //编写一个函数，接收两个整数，然后返回它们的和、积与差。编写两个版本，一个是非命名返回值，一个是命名返回值。
+
+// func cal_name(a,b int)(he int,cha int,ji int){
+// 	return a+b,a-b,a*b
+// }
+
+// func cal(a,b int) (int,int,int){
+// 	return a+b,a-b,a*b
+// }
+
+
+// func main(){
+// 	fmt.Println(cal_name(1,2))
+// 	fmt.Println(cal(1,2))
+// }
